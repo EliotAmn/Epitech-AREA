65e805822182691613907da56801cd71b6cd535e
Eliot <contact@eliotamanieu.fr>
2025-11-30 21:55:46 +0100

    feat(backend): enhance service types with parameter definitions and action classes

--- backend/src/common/service.types.ts
+++ backend/src/common/service.types.ts
@@ -1,23 +1,55 @@
-export interface UserAction {
 
+export enum ParamaterType {
+    STRING = "string",
+    NUMBER = "number",
+    BOOLEAN = "boolean",
+    SELECT = "select",
 }
 
-export interface ServiceAction {
+export interface ParameterValue {
+    type: ParamaterType;
+    value: any;
+}
+
+export interface ParameterDefinition {
+    name: string;
+    type: ParamaterType;
+    label: string;
+    description: string;
+    required: boolean;
+    options?: string[]; // For SELECT type
+}
+
+interface ServiceConfig {
+    config: Record<string, any>;
+}
+
+export class ServiceActionDefinition {
     name: string;
     description: string;
-    parameters: Record<string, any>;
+    output_params: ParameterDefinition[];
+
+    // Function to reload the cache of the action in a user-area context
+    reload_cache: (sconf: ServiceConfig) => Promise<Record<string, any>>;
+
+    poll: (sconf: ServiceConfig) => Promise<{triggered: boolean, parameters: Record<string, ParameterValue>}>;
+
 }
 
-export interface ServiceReaction {
+export class ServiceReactionDefinition {
     name: string;
     description: string;
-    parameters: Record<string, any>;
+    input_params: ParameterDefinition[];
+
+    // sconf: Service config, params: parameters/context given from the area (not the action params definition)
+    execute: (sconf: ServiceConfig, params: Record<string, any>) => Promise<void>;
 }
+
 
 export interface ServiceDefinition {
     name: string;
     label: string;
     description: string;
-    actions: ServiceAction[];
-    reactions: ServiceReaction[];
+    actions: ServiceActionDefinition[];
+    reactions: ServiceReactionDefinition[];
 }
