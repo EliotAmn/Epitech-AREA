4ff3811b372062d9ed71bf455967c8cee4958bae
Eliot <contact@eliotamanieu.fr>
2025-11-30 22:43:38 +0100

    feat(backend): update service paths and implement UserService module and service

---
+++ backend/src/modules/user_service/userservice.service.ts
@@ -0,0 +1,12 @@
+import {Injectable} from "@nestjs/common";
+
+@Injectable()
+export class UserServiceService {
+
+    async save_service_cache(userservice_id: string, to_cache: Record<string, any>): Promise<void> {
+
+    }
+    
+
+
+}
