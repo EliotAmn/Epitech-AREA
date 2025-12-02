6752eecef965b24f2e871432f99ac093868907cd
Eliot <contact@eliotamanieu.fr>
2025-12-02 14:29:40 +0100

    feat(auth): fix import path for UserService in auth.service.ts

--- backend/src/modules/auth/auth.service.ts
+++ backend/src/modules/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
-import {UserService} from "../../user/user.service";
 import {PasswordService} from "../common/password/password.service";
+import {UserService} from "../user/user.service";
 
 @Injectable()
 export class AuthService {
