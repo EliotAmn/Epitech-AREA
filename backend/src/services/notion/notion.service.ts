c65fad660c3f29c54b8f02c2b1489fc12c93d284
Morgan <morgan.guichard@epitech.eu>
2026-01-12 16:44:16 +0100

    feat: add colors and logo

--- backend/src/services/notion/notion.service.ts
+++ backend/src/services/notion/notion.service.ts
@@ -107,6 +107,9 @@ export default class NotionService implements ServiceDefinition {
     redirect_uri: buildServiceRedirectUrl('notion'),
   });
   oauth_callback = oauth_callback;
+  logo =
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e9/Notion-logo.svg/3840px-Notion-logo.svg.png';
+  color = '#000000';
   description =
     'Connect your Notion workspace to automate tasks and integrate with your notes, databases, and pages.';
   actions = [PageCreated, PageUpdated];
