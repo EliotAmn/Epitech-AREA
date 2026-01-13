5b07517be8be611e61a141beec6d5992eb63ffb0
jserygit <julien.sery@epitech.eu>
2026-01-13 11:41:25 +0100

    fix: phrasing

--- mobile/lib/pages/create_area/reaction_page.dart
+++ mobile/lib/pages/create_area/reaction_page.dart
@@ -24,7 +24,7 @@ class ReactionPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          'Choose reaction ',
+          'Choose a reaction',
           style: Theme.of(context).textTheme.displayLarge,
         ),
       ),
