258c7d8d079fd8102154a52a78038383e7404be9
Ambre Cornejo <ambre.cornejo@epitech.eu>
2026-01-17 11:48:45 +0100

    fix: lint

--- backend/src/common/guards/admin.guard.ts
+++ backend/src/common/guards/admin.guard.ts
@@ -4,11 +4,14 @@ import {
   ForbiddenException,
   Injectable,
 } from '@nestjs/common';
+import { Request } from 'express';
 
 @Injectable()
 export class AdminGuard implements CanActivate {
   canActivate(context: ExecutionContext): boolean {
-    const request = context.switchToHttp().getRequest();
+    const request = context
+      .switchToHttp()
+      .getRequest<Request & { user?: { admin: boolean } }>();
     const user = request.user;
 
     if (!user || user.admin !== true) {
